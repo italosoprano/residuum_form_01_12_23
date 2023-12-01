1c156ad98d724020f35eecdadd75a20417f22c36
Ítalo Soprano <110266373+italosoprano@users.noreply.github.com>
2023-12-01 15:30:49 -0400

    update no formulario
    updates feitos conforme foram solicitados.

--- index.js
+++ index.js
@@ -1,69 +1,10 @@
-//imagem do header para reload
-const HeaderImg = document.querySelector("#HeaderImg")
+const PaperType = document.querySelector("#PaperType")
+const MetalType = document.querySelector("#MetalType")
+const PlasticType = document.querySelector("#PlasticType")
+const GlassType = document.querySelector("#GlassType")
+const OrgType = document.querySelector("#OrgType")
+const inputCadastroName = document.querySelector("#inputCadastroName")
+const inputCadastroCelular = document.querySelector("#inputCadastroCelular")
+const inputTextArea = document.querySelector("#inputTextArea")
+const btn_Enviar = document.querySelector("#inputCadastroEnviar")
 
-HeaderImg.addEventListener("click", (e)=>{
-    location.reload()
-})
-
-//botao do formulario de cadastro
-const btn_inputCadastroEnviar = document.querySelector("#inputCadastroEnviar")
-
-//função que recebe os valores do formulario de cadastro
-btn_inputCadastroEnviar.addEventListener("click", (e)=>{
-    e.preventDefault()
-
-    //inputs do formulario de cadastro
-    const inputCadastroName = document.querySelector("#inputCadastroName")
-    const inputCadastroCelular = document.querySelector("#inputCadastroCelular")
-    const inputCadastroEmail = document.querySelector("#inputCadastroEmail")
-    const inputCadastroDateNasc = document.querySelector("#inputCadastroDateNasc")
-    const inputCadastroEscolaridade = document.querySelector("#inputCadastroEscolaridade")
-    const inputCadastroEndereco = document.querySelector("#inputCadastroEndereco")
-
-    let Nome = inputCadastroName.value
-    let Celular =  inputCadastroCelular.value
-    let Email = inputCadastroEmail.value
-    let DateNasc = inputCadastroDateNasc.value;
-    let Escolaridade = inputCadastroEscolaridade.value;
-    let Endereco = inputCadastroEndereco.value;
-
-    //para confirmar a entrada dos dados.
-    console.log(Nome)
-    console.log(Celular)
-    console.log(Email)
-    console.log(DateNasc)
-    console.log(Escolaridade)
-    console.log(Endereco)
-
-    //para retornar o valor da matricula para o usuario. 
-    //alert()
-
-})
-
-
-/*------------------------------------------------------------*/
-
-//botao do formulario de descarte
-const btn_inputDescarteEnviar = document.querySelector("#btn_inputDescarteEnviar")
-
-//função que recebe os valores do formulario de cadastro
-btn_inputDescarteEnviar.addEventListener("click", (e)=>{
-    e.preventDefault()
-
-    //inputs do formulario de descarte
-    const inputDescarteMatricula = document.querySelector("#inputDescarteMatricula")
-    const inputDescarteDateToday = document.querySelector("#inputDescarteDateToday")
-    const inputDescarteType = document.querySelector("#inputDescarteType")
-    const inputDescarteWeight = document.querySelector("#inputDescarteWeight")
-
-    let Matricula = inputDescarteMatricula.value
-    let DateToday = inputDescarteDateToday.value
-    let DescarteType = inputDescarteType.value
-    let Weight = inputDescarteWeight.value
-
-    //para confirmar a entrada dos dados.
-    console.log(Matricula)
-    console.log(DateToday)
-    console.log(DescarteType)
-    console.log(Weight)
-})
